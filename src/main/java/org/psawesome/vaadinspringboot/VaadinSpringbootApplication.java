e31f3d93c6fc21dbe5d9a0a6e064a37eb1178f80
PSK <wiv821253@gmail.com>
2023-03-25 14:59:33 +0900

    Initial commit

---
+++ src/main/java/org/psawesome/vaadinspringboot/VaadinSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.psawesome.vaadinspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/psawesome/vaadinspringboot/VaadinSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.psawesome.vaadinspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
